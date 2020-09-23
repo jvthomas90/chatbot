9ecb570d8203e912bf1bb9300fca6154b502be38
jvthomas90 <69699907+jvthomas90@users.noreply.github.com>
2020-09-22 23:05:52 -0500

    Update chatbot.py
    Finished "quotes" and "productivity" responses and fine-tuned with proper syntax
    
    ...still debating whether to include the "music" responses from my pseudocode in the final product 
    (and if so, should I keep the print()+webbrowser.open() format, or opt to comply with the "return" requirement of the assignment instead? decisions, decisions...)

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,20 @@
+from random import choice
+
 def get_bot_response(user_response):
 	if "quote" in user_response:
-		return random.choise[pearlsOfWisdom,list]
+		return choice["Live long and prosper 🖖🏼", "2 plus 2 is 4, minus 1 that's 3 - quick maths!", "Never run with scissors!!! ε=ε=ε=(╯°□°)╯✂️"]
 	elif "produvtiv" in user_response:
-		prod_topic = random.choice(gtd,xyz)
-		if prod_topic == "gtd"
-			print(...)
-			web.open(resourceURL)
+		prod_topic = random.choice("gtd","priority","autofocus","pomodoro")
+		if prod_topic == "gtd":
+			return "Read 'Getting Things Done' by David Allen to improve your project-planning methodology 🗂📈"
+        elif prod_topic == "priority":
+            return "Learn about Eisenhower's 'priority-matrix' method for decision-making! ◳🤔"
+        elif prod_topic == "autofocus":
+            return "Single-task effectively with Mark Forster's 'AutoFocus' list technique ✅📝"
+        elif prod_topic == "pomodoro":
+            return "For peak-performance, your brain needs to relax in 5-15 minute diffuse mode time-blocks in between 30 minute focused-work sessions ⏳🧠"
 	elif "music" in user_response:
-		tunes = random[classical,scatt,whatever]
+		tunes = random["classical","scatt","edm"]
 		if tunes == "classical"
 			print("Confutatis movement of Mozart's 'Requiem in D minor'"")
 			choice = input("Wanna listen? Y/N")
