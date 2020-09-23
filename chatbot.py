6e2c48d313e7a22cf29b1d6b38f8186be7902cb2
jvthomas90 <69699907+jvthomas90@users.noreply.github.com>
2020-09-22 22:22:39 -0500

    Create chatbot.py
    Simple pseudocode ideation/outline for now

---
+++ chatbot.py
@@ -0,0 +1,35 @@
+def get_bot_response(user_response):
+	if "quote" in user_response:
+		return random.choise[pearlsOfWisdom,list]
+	elif "produvtiv" in user_response:
+		prod_topic = random.choice(gtd,xyz)
+		if prod_topic == "gtd"
+			print(...)
+			web.open(resourceURL)
+	elif "music" in user_response:
+		tunes = random[classical,scatt,whatever]
+		if tunes == "classical"
+			print("Confutatis movement of Mozart's 'Requiem in D minor'"")
+			choice = input("Wanna listen? Y/N")
+				if choice.upper() == "Y"
+					web.open()
+				else:
+					pass/break/continue?
+		elif tunes == "scatt"
+			web.open(scattman)
+			return "gibberish lorem ipsum here"
+		else:
+			???
+	else
+		return random.choice[default,responses,here]
+
+user_response = ""
+
+while True:
+	user_response = input(prompt)
+
+	if user_response = "done"
+		break
+	else
+		get_bot_Response(user_response)
+		continue
